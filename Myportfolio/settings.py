cbbe92bae9f619ff4309b7199cddfd3a4b139dc2
shalini.yadav <shalu230927@gmail.com>
2025-02-04 18:22:20 +0530

    initial commit

--- Myportfolio/settings.py
+++ Myportfolio/settings.py
@@ -123,6 +123,9 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# For storing static files during the build process
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
